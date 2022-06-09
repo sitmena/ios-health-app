31080d04ebda39819a5bba9f6e95f9abc0fe6547
Mohammad Salhab <mohammad.s@sitech.me>
2022-06-09 16:44:27 +0300

    commit files and folders

---
+++ Health/Data/APIEndPoints.swift
@@ -0,0 +1,21 @@
+//
+//  APIEndPoints.swift
+//  Health
+//
+//  Created by Mohammad Salhab on 09/06/2022.
+//
+
+import Foundation
+
+struct APIEndpoints {
+    
+//    static func getData(with moviesRequestDTO: MoviesRequestDTO) -> Endpoint<MoviesResponseDTO> {
+//
+//        return Endpoint(path: "3/search/movie/",
+//                        method: .get,
+//                        queryParametersEncodable: moviesRequestDTO)
+//    }
+
+    
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Health' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Health
+
+ pod "Resolver"
+
+end
